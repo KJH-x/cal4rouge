945eaace64d8aaf1eb7cc83502ad08c6105bcc17
KJH-x <53437291+KJH-x@users.noreply.github.com>
2024-08-14 17:26:52 +0800

    Fix: load params from url search

--- js/load-panel.js
+++ js/load-panel.js
@@ -1,128 +1,138 @@
 document.addEventListener("DOMContentLoaded", function () {
     let totalScore = 0;
+    let panelList = [];
     const logArea = document.getElementById("logArea");
     const totalScoreValue = document.getElementById("totalScoreValue");
 
     fetch('/assets/configs/panels.json')
         .then(response => response.json())
-        .then(data => generatePanels(data));
+        .then(data => {
+            generatePanels(data);
+            loadStateFromParams();
+        });
 
     function generatePanels(data) {
         const container = document.getElementById("leftContent");
 
         data["block-list"].forEach(panel => {
-            const panelDiv = document.createElement("div");
-            panelDiv.className = "panel";
-
-            // Title Box
-            const titleBox = document.createElement("div");
-            titleBox.className = "panel-title-box";
-            const title = document.createElement("span");
-            title.className = "panel-title";
-            title.textContent = panel.title;
-            const subtitle = document.createElement("span");
-            subtitle.className = "panel-subtitle";
-            subtitle.textContent = panel.subtitle;
-            titleBox.appendChild(title);
-            titleBox.appendChild(subtitle);
-            panelDiv.appendChild(titleBox);
-
-            // Items
-            panel.items.forEach(item => {
-                const itemDiv = document.createElement("div");
-                itemDiv.className = "panel-item";
-
-                const img = document.createElement("img");
-                img.src = item.pics;
-                itemDiv.appendChild(img);
-
-                const itemName = document.createElement("span");
-                itemName.className = "item-name";
-                itemName.textContent = item.name;
-                itemDiv.appendChild(itemName);
-
-                const operationDiv = document.createElement("div");
-                operationDiv.className = "operation";
-
-                if (item.input.type === "int") {
-                    const minusButton = document.createElement("button");
-                    minusButton.innerHTML = '<svg width="20px" height="20px"><use xlink:href="#icon-minus"></use></svg>';
-                    const plusButton = document.createElement("button");
-                    plusButton.innerHTML = '<svg width="20px" height="20px"><use xlink:href="#icon-plus"></use></svg>';
-                    const inputField = document.createElement("input");
-                    inputField.type = "text";
-                    inputField.value = item.input.offset;
-                    inputField.className = "item-count"
-                    inputField.value = item.input.default !== undefined ? item.input.default : -item.input.offset;
-
-                    minusButton.addEventListener("click", function () {
-                        if (parseInt(inputField.value) > 0) {
-                            inputField.value = parseInt(inputField.value) - 1;
-                            updateSummary(panel, panelDiv);
-                            logAction(item.name, '-1');
-                        }
-                    });
-
-                    plusButton.addEventListener("click", function () {
-                        inputField.value = parseInt(inputField.value) + 1;
-                        updateSummary(panel, panelDiv);
-                        logAction(item.name, '+1');
-                    });
-
-                    operationDiv.appendChild(minusButton);
-                    operationDiv.appendChild(inputField);
-                    operationDiv.appendChild(plusButton);
-                } else if (item.input.type === "bool") {
-                    const switchDiv = document.createElement("div");
-                    switchDiv.className = "switch";
-                    if (item.input.default == true) {
-                        switchDiv.classList.add("active");
-                    }
-
-                    const sliderDiv = document.createElement("div");
-                    sliderDiv.className = "slider";
-                    switchDiv.appendChild(sliderDiv);
-
-                    switchDiv.addEventListener("click", function () {
-                        switchDiv.classList.toggle("active");
-                        updateSummary(panel, panelDiv);
-                        logAction(item.name, switchDiv.classList.contains("active") ? 'ON' : 'OFF');
-                    });
-
-                    operationDiv.appendChild(switchDiv);
-                }
-
-                itemDiv.appendChild(operationDiv);
-                panelDiv.appendChild(itemDiv);
-            });
-
-            // Summary Box
-            // Summary Box with two equal divs for label and value
-            const summaryBox = document.createElement("div");
-            summaryBox.className = "panel-summary-box";
-
-            const labelContainer = document.createElement("div");
-            labelContainer.className = "summary-container";
-            const valueContainer = document.createElement("div");
-            valueContainer.className = "summary-container";
-
-            const label = document.createElement("span");
-            label.className = "summary-label";
-            label.textContent = "本项总分";
-            labelContainer.appendChild(label);
-
-            const value = document.createElement("span");
-            value.className = "summary-value";
-            valueContainer.appendChild(value);
-
-            summaryBox.appendChild(labelContainer);
-            summaryBox.appendChild(valueContainer);
-
-            panelDiv.appendChild(summaryBox);
+            const panelDiv = createPanel(panel)
+            panelList.push({ panel, panelDiv });
             container.appendChild(panelDiv);
-
             updateSummary(panel, panelDiv);
         });
+        loadStateFromParams();
+    }
+    function createPanel(panel) {
+        const panelDiv = document.createElement("div");
+        panelDiv.className = "panel";
+
+        // Title Box
+        const titleBox = document.createElement("div");
+        titleBox.className = "panel-title-box";
+        const title = document.createElement("span");
+        title.className = "panel-title";
+        title.textContent = panel.title;
+        const subtitle = document.createElement("span");
+        subtitle.className = "panel-subtitle";
+        subtitle.textContent = panel.subtitle;
+        titleBox.appendChild(title);
+        titleBox.appendChild(subtitle);
+        panelDiv.appendChild(titleBox);
+
+        // Items
+        panel.items.forEach(item => {
+            const itemDiv = document.createElement("div");
+            itemDiv.className = "panel-item";
+
+            const img = document.createElement("img");
+            img.src = item.pics;
+            itemDiv.appendChild(img);
+
+            const itemName = document.createElement("span");
+            itemName.className = "item-name";
+            itemName.textContent = item.name;
+            itemDiv.appendChild(itemName);
+
+            const operationDiv = document.createElement("div");
+            operationDiv.className = "operation";
+
+            if (item.input.type === "int") {
+                const minusButton = document.createElement("button");
+                minusButton.innerHTML = '<svg width="20px" height="20px"><use xlink:href="#icon-minus"></use></svg>';
+                const plusButton = document.createElement("button");
+                plusButton.innerHTML = '<svg width="20px" height="20px"><use xlink:href="#icon-plus"></use></svg>';
+                const inputField = document.createElement("input");
+                inputField.type = "text";
+                inputField.value = item.input.offset;
+                inputField.className = "item-count"
+                inputField.value = item.input.default !== undefined ? item.input.default : -item.input.offset;
+
+                minusButton.addEventListener("click", function () {
+                    if (parseInt(inputField.value) > 0) {
+                        inputField.value = parseInt(inputField.value) - 1;
+                        updateSummary(panel, panelDiv);
+                        logAction(item.name, '-1');
+                    }
+                });
+
+                plusButton.addEventListener("click", function () {
+                    inputField.value = parseInt(inputField.value) + 1;
+                    updateSummary(panel, panelDiv);
+                    logAction(item.name, '+1');
+                });
+
+                operationDiv.appendChild(minusButton);
+                operationDiv.appendChild(inputField);
+                operationDiv.appendChild(plusButton);
+            } else if (item.input.type === "bool") {
+                const switchDiv = document.createElement("div");
+                switchDiv.className = "switch";
+                if (item.input.default == true) {
+                    switchDiv.classList.add("active");
+                }
+
+                const sliderDiv = document.createElement("div");
+                sliderDiv.className = "slider";
+                switchDiv.appendChild(sliderDiv);
+
+                switchDiv.addEventListener("click", function () {
+                    switchDiv.classList.toggle("active");
+                    updateSummary(panel, panelDiv);
+                    logAction(item.name, switchDiv.classList.contains("active") ? 'ON' : 'OFF');
+                });
+
+                operationDiv.appendChild(switchDiv);
+            }
+
+            itemDiv.appendChild(operationDiv);
+            panelDiv.appendChild(itemDiv);
+        });
+
+        // Summary Box
+        // Summary Box with two equal divs for label and value
+        const summaryBox = document.createElement("div");
+        summaryBox.className = "panel-summary-box";
+
+        const labelContainer = document.createElement("div");
+        labelContainer.className = "summary-container";
+        const valueContainer = document.createElement("div");
+        valueContainer.className = "summary-container";
+
+        const label = document.createElement("span");
+        label.className = "summary-label";
+        label.textContent = "本项总分";
+        labelContainer.appendChild(label);
+
+        const value = document.createElement("span");
+        value.className = "summary-value";
+        valueContainer.appendChild(value);
+
+        summaryBox.appendChild(labelContainer);
+        summaryBox.appendChild(valueContainer);
+
+        panelDiv.appendChild(summaryBox);
+
+        return panelDiv
     }
 
     function updateSummary(panel, panelDiv) {
@@ -155,11 +165,9 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         sum = sum * panel.output.factor + panel.output.offset;
-
         const summaryValue = panelDiv.querySelector(".summary-value");
         summaryValue.textContent = sum;
 
-        // Update total score
         totalScore += sum;
         totalScoreValue.textContent = totalScore;
     }
@@ -172,5 +180,56 @@ document.addEventListener("DOMContentLoaded", function () {
         logArea.appendChild(logElement);
         logArea.scrollTop = logArea.scrollHeight; // Scroll to the bottom
     }
-    loadStateFromParams();
+
+
+    function updateState(encodedString) {
+        try {
+            const stateString = atob(encodedString);
+            const [versionString, states] = stateString.split(':');
+            const stateArray = states.split(';');
+            const currentVersion = document.getElementById('versionInfo').textContent;
+
+            if (versionString !== currentVersion) {
+                console.log('Version mismatch. State not updated.');
+                return;
+            }
+            console.log(versionString, states, stateArray, stateArray.length)
+
+            let stateIndex = 0;
+            panelList.forEach(({ panel, panelDiv }) => {
+                const items = panelDiv.querySelectorAll('.panel-item');
+                items.forEach((itemDiv, index) => {
+                    const item = panel.items[index];
+                    if (stateIndex < stateArray.length) {
+                        const stateValue = stateArray[stateIndex];
+                        if (item.input.type === 'int') {
+                            const inputField = itemDiv.querySelector('input');
+                            inputField.value = Math.max(0, parseInt(stateValue));
+                        } else if (item.input.type === 'bool') {
+                            const switchDiv = itemDiv.querySelector('.switch');
+                            if (stateValue === '1') {
+                                switchDiv.classList.add('active');
+                            } else {
+                                switchDiv.classList.remove('active');
+                            }
+                        }
+                        stateIndex++;
+                    }
+                });
+                updateSummary(panel, panelDiv); // 每次更新后重新计算summary
+            });
+
+        } catch (e) {
+            console.error('Failed to decode state:', e);
+        }
+    }
+
+
+    function loadStateFromParams() {
+        const params = new URLSearchParams(window.location.search);
+        const encodedState = params.get('s');
+        if (encodedState) {
+            updateState(encodedState);
+        }
+    }
 });
--- js/page-utils.js
+++ js/page-utils.js
@@ -17,70 +17,26 @@ function getStateString() {
         });
     });
 
-    stateString = stateString.slice(0, -1); 
-    
+    stateString = stateString.slice(0, -1);
+
     const encodedState = btoa(stateString)
     document.getElementById('currentStatus').textContent = encodedState
     return encodedState;
 }
 
-function updateState(encodedString) {
-    try {
-        const stateString = atob(encodedString);
-        const [versionString, states] = stateString.split(':');
-        const stateArray = states.split(';');
-        const currentVersion = document.getElementById('versionInfo').textContent;
-
-        if (versionString !== currentVersion) {
-            console.log('Version mismatch. State not updated.');
-            return;
-        }
-
-        let stateIndex = 0;
-        const panels = document.querySelectorAll('.panel');
-        panels.forEach(panel => {
-            const items = panel.querySelectorAll('.panel-item');
-            items.forEach(itemDiv => {
-                const inputField = itemDiv.querySelector('input');
-                const switchDiv = itemDiv.querySelector('.switch');
-                if (stateIndex < stateArray.length) {
-                    const stateValue = stateArray[stateIndex];
-                    if (inputField) {
-                        inputField.value = Math.max(0, parseInt(stateValue));
-                    } else if (switchDiv) {
-                        if (stateValue === '1') {
-                            switchDiv.classList.add('active');
-                        } else {
-                            switchDiv.classList.remove('active');
-                        }
-                    }
-                    stateIndex++;
-                }
-            });
-        });
-
-    } catch (e) {
-        console.error('Failed to decode state:', e);
-    }
-}
-
-
 function copyState() {
     const encodedState = getStateString();
+    const currentUrl = new URL(window.location.href);
+    const params = currentUrl.searchParams;
+
+    params.delete('s');
+    params.set('s', encodedState);
+    NewUrl = `${window.location.origin}${currentUrl.pathname}?${params.toString()}`;
+
     document.getElementById('currentStatus').textContent = encodedState;
-    navigator.clipboard.writeText(encodedState).then(function () {
+    navigator.clipboard.writeText(NewUrl).then(function () {
         alert('State copied to clipboard!');
     }).catch(function (err) {
         alert('Failed to copy state: ' + err);
     });
 }
-
-function loadStateFromParams() {
-    const params = new URLSearchParams(window.location.search);
-    const encodedState = params.get('check');
-    if (encodedState) {
-        updateState(encodedState);
-        document.getElementById('currentStatus').value = encodedState;
-    }
-}
-
